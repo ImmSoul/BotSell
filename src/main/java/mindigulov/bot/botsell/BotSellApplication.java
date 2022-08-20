f5ee1a3ad62a0d20ae0788b16a71360021fdb84a
ImmSoul <silver-oniks@list.ru>
2022-08-20 19:32:24 +0700

    Initial commit

---
+++ src/main/java/mindigulov/bot/botsell/BotSellApplication.java
@@ -0,0 +1,13 @@
+package mindigulov.bot.botsell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotSellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotSellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mindigulov/bot/botsell/BotSellApplicationTests.java
@@ -0,0 +1,13 @@
+package mindigulov.bot.botsell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotSellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
